46b11a3b83aa99a80199c41383f8dcd247ff7f7e
dhanushd-27 <dhanushsai2005@gmail.com>
2025-03-27 15:59:10 +0530

    Add: Create User Route &&

--- apps/http/src/index.ts
+++ apps/http/src/index.ts
@@ -1,11 +1,29 @@
-import express from 'express';
+import { prisma } from '@repo/db/prisma';
+import express, { Request, Response } from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Hello, World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.post('/signup', async (req: Request, res: Response) => {
+  const { name, email } = req.body;
+
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+    },
+  });
+
+  res.json({ 
+    message: 'User created successfully',
+    user,
+  });
+});
+
+app.listen(3007, () => {
+  console.log('Server is running on http://localhost:3007');
 });
